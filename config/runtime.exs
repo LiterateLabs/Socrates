bf9f04db537039bdf3919c7d9612552d189603a2
Jon <tadasajon@gmail.com>
2021-09-27 11:11:57 -0400

    prepping for deployment on Fly.io

--- config/runtime.exs
+++ config/runtime.exs
@@ -24,11 +24,18 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
   config :socrates, Socrates.Repo,
     # ssl: true,
     # socket_options: [:inet6],
     url: database_url,
+    # IMPORTANT: Or it won't find the DB server
+    socket_options: [:inet6],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
@@ -38,24 +45,13 @@ if config_env() == :prod do
       """
 
   config :socrates, SocratesWeb.Endpoint,
+    server: true,
+    url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      # IMPORTANT: support IPv6 addresses
+      transport_options: [socket_opts: [:inet6]]
     ],
     secret_key_base: secret_key_base
-
-  # ## Using releases
-  #
-  # If you are doing OTP releases, you need to instruct Phoenix
-  # to start each relevant endpoint:
-  #
-  #     config :socrates, SocratesWeb.Endpoint, server: true
-  #
-  # Then you can assemble a release by calling `mix release`.
-  # See `mix help release` for more information.
 
 end
